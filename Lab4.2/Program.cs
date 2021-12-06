4620664cf78faeeba7dcb076f6254f04bec4781a
Skitt2 <provaid@yandex.ru>
2021-12-07 00:00:31 +0300

    Добавлена задача 2 , 3

---
+++ Lab4.2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int positive = 0;
+            int negative = 0;
+            int number;
+            Console.WriteLine("Осуществить ввод последовательности целых чисел и сравнить, что больше, количество положительных или количество отрицательных. Последовательность потенциально не ограничена, окончанием последовательности служит число 0.");
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                {
+                    break;
+                }
+                if (number > 0)
+                {
+                    positive++;
+                }
+                else
+                {
+                    negative++;
+                }
+                if (positive == negative)
+                {
+                    Console.WriteLine("Больше положительных чисел равно числу отрицательных чисел {0} = {1}", positive, negative);
+                    continue;
+                }
+                if (positive > negative)
+                {
+                    Console.WriteLine("Больше положительных чисел {0} > {1}", positive, negative);
+                }
+                else
+                {
+                    Console.WriteLine("Больше отрицательных чисел {1} > {0}", positive, negative);
+                }
+            } while (number != 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab4.3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести положительные числа A, B, C. На прямоугольнике размера A x B размещено максимально возможное количество квадратов со стороной C (без наложений). Найти количество квадратов, размещенных на прямоугольнике. Операции умножения и деления не использовать.");
+            Console.Write("A=");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int firstA = a;
+            Console.Write("B=");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("C=");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int x = 0; // Счетчик количества размещенных квадратов
+            while (a > c && b > c)
+            {
+                a -= c;
+                ++x;
+                if (a <= c && b > c)
+                {
+                    b -= c;
+                    a = firstA;
+                }
+            }
+            if (x == 0)
+            {
+                Console.WriteLine("Не одного вхождения квадрата в прямоугольник");
+            }
+            else
+            {
+                Console.WriteLine("Количество квадратов, размещенных на прямоугольнике равно {0}", x);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
